0181b3f9b776735c9cde76a37ccedf855392c5c8
Bhon Bunnag <bbunnag@wpi.edu>
2017-10-16 00:25:40 -0400

    add basic attack

---
+++ Assets/AWeapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AWeapon : MonoBehaviour {
+
+	float range; // how far the range of attack is
+	float impactFactor; //how much force will be applied to the enemy player 
+	int numUse; //how many times can use before it dissapears
+
+	void Start () {
+		//Position randomly in the world
+		//associate with an image
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//maybe make the thing turn or light up
+	}
+
+	public abstract void activate (); //Fire stufff
+
+}
---
+++ Assets/WeaponSlot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSlot : MonoBehaviour {
+
+	public GameObject defaultWeaponPF;
+	public AWeapon weapon;
+	void Start () {
+		GameObject temp;
+		temp = Instantiate (defaultWeaponPF, transform.position, transform.rotation);
+		PickUp (temp.GetComponent<AWeapon>());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			weapon.activate();
+		}
+	}
+
+	void PickUp(AWeapon w){
+		weapon = w;
+		w.transform.parent = transform;
+		w.transform.localPosition = Vector3.zero;
+	}
+}
