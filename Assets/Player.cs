e8700720c3826cf471a595cc43b40c7aaa2bd2d3
Vincent Miller <vmiller0725@gmail.com>
2017-10-17 00:26:25 -0400

    checkpoints

---
+++ Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private Transform lastCheckpoint;
+    private CharacterMotor2D motor;
+    public float stunTime = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+        motor = GetComponent<CharacterMotor2D>();
+	}
+	
+	// Update is called once per frame
+	
+    void EnableMovement() {
+        motor.enabled = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Checkpoint")) {
+            lastCheckpoint = collision.transform;
+        } else if (collision.CompareTag("Spikes")) {
+            transform.position = lastCheckpoint.position;
+            motor.velocity = Vector3.zero;
+            motor.enabled = false;
+            Invoke("EnableMovement", stunTime);
+        }
+    }
+}
